bab13e9f84032abad6488823fe73809c2f8fa489
Alexandr Melnichenko <alexandr.melnichenko@gmail.com>
2024-08-28 22:22:25 +0300

    ready hw-8

--- js/task-1.js
+++
@@ -1,8 +0,0 @@
-const itemCategories = document.querySelectorAll("#categories > .item");
-console.log(`Number of categories: ${itemCategories.length}`);
-
-itemCategories.forEach(category => {
-  console.log(`Category: ${category.querySelector("h2").textContent}`);
-  console.log(`Elements: ${category.querySelectorAll("ul > li").length}`);
-});
-
